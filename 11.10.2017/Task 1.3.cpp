79067db3591c7c2b23575a09591d461335bd0891
PolinaVoloshchik <polyapolya2000@gmail.com>
2017-10-17 22:01:57 +0300

    Add Task 1.3

---
+++ 11.10.2017/Task 1.3.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+double sinTaylor(double, double);
+
+int main()
+{
+	double a, b;
+	cout << "Enter a = " ;
+	cin >> a;
+	while (true)
+	{
+		cout << "Enter b > a: ";
+		cin >> b;
+		if (b > a) break;
+		cout << "Invalid data! Try again" << endl;
+	}
+	double x, eps;
+	while (true)
+	{
+		cout << "Enter 0 < epsilon < 1 : ";
+		cin >> eps;
+		if (eps > 0 && eps < 1)
+			break;
+		cout << "Invalid data! Try again!" << endl;
+	}
+	double h;
+	
+	while (true)
+	{
+		cout << "Enter h < b - a: ";
+		cin >> h;
+		if (h < b - a) break;
+		cout << "Invalid data! Try again" << endl;
+	}
+	for (x = a; x < b; x = x + h)
+	{
+		cout << "x = " << x << "  ";
+		cout << "sum = " << sinTaylor(x, eps) << "  ";
+		cout << "sin = " << sin(x) << endl;
+	}
+	system("pause");
+}
+
+double sinTaylor(double x, double eps)
+{
+	double term = x, sum = 0;
+	for (int i = 1; fabs(term) > eps; i++)
+	{
+		sum += term;
+		term = -term * x * x / (2 * i) / (2 * i + 1);
+	}
+	return sum;
+}
